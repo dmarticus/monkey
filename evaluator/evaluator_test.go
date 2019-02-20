2e7f710fa8bc7c3a99385815ff5b52c07eee5d42
Dylan Martin <dylanm@qualtrics.com>
2019-02-19 21:59:32 -0800

    omfg that error message was driving me crazy

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -1,29 +1,33 @@
 package evaluator
 
-import ( 
+import (
+	"testing"
+
+	"github.com/dmarticus/monkey/ast"
 	"github.com/dmarticus/monkey/lexer"
-    "github.com/dmarticus/monkey/object"
-    "github.com/dmarticus/monkey/parser"
-    "testing"
+	"github.com/dmarticus/monkey/object"
+	"github.com/dmarticus/monkey/parser"
 )
-func TestEvalIntegerExpression(t *testing.T) { 
+
+func TestEvalIntegerExpression(t *testing.T) {
 	tests := []struct {
-		input string 
+		input    string
 		expected int64
 	}{
 		{"5", 5},
-		{"10", 10}, 
+		{"10", 10},
 	}
 	for _, tt := range tests {
-		evaluated := testEval(tt.input) testIntegerObject(t, evaluated, tt.expected)
-	} 
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
 }
 
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
 	program := p.ParseProgram()
-	
+
 	return Eval(program)
 }
 
@@ -32,10 +36,18 @@ func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
 	if !ok {
 		t.Errorf("object is not Integer, got=%T (%v) instead", obj, obj)
 		return false
-	} 
+	}
 	if result.Value != expected {
 		t.Errorf("object has wrong value; got=%d, want=%d", result.Value, expected)
 		return false
 	}
 	return true
+}
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+	return nil
 }
